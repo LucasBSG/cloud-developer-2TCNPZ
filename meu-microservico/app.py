c5072055644d139d9eb78f6d064d67b671103473
Roberta <roberta@MacBook-Pro-de-Roberta.local>
2025-03-06 20:43:53 -0300

    push roberta

---
+++ meu-microservico/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Microserviço rodando!"
+
+@app.route('/status')
+def status():
+    return jsonify({"status": "ok"})
+
+
+@app.route('/info')
+def info():
+    return jsonify({"info": "Meu microservico de teste"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
