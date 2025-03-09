02c5468938a7580ae8a31703eabc0b2476360849
hugogrillo <hugogrillo.a@gmail.com>
2025-03-09 17:13:23 -0300

    versaoteste

--- projetos/e-commerce/usuarios-service/app.py
+++ projetos/e-commerce/usuarios-service/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+# Lista para armazenar os usuários
+usuarios = []
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        # Obter dados do usuário do corpo da solicitação
+        novo_usuario = request.get_json()
+        # Adicionar o novo usuário à lista
+        usuarios.append(novo_usuario)
+        return jsonify({"mensagem": "Usuário cadastrado com sucesso!"}), 201
     return "Microserviço rodando!"
 
 @app.route('/status')
