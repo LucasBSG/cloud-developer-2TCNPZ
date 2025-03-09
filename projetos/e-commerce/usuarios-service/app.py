5a67881cbeaaf65c25dcda8542f8402068a0d834
renan <rm93744@fiap.com.br>
2025-03-09 17:28:10 -0300

    Versao CP1

--- projetos/e-commerce/usuarios-service/app.py
+++ projetos/e-commerce/usuarios-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template_string
 
 app = Flask(__name__)
 
@@ -8,20 +8,26 @@ usuarios = []
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Obter dados do usuário do corpo da solicitação
-        novo_usuario = request.get_json()
+        # Obter dados do usuário do formulário
+        nome = request.form['nome']
+        email = request.form['email']
+        novo_usuario = {"nome": nome, "email": email}
         # Adicionar o novo usuário à lista
         usuarios.append(novo_usuario)
-        return jsonify({"mensagem": "Usuário cadastrado com sucesso!"}), 201
-    return "Microserviço rodando!"
+        return jsonify({"mensagem": "Usuario cadastrado com sucesso!"}), 201
+    return render_template_string('''
+        <h1>Cadastro de Usuários</h1>
+        <form method="post">
+            Nome: <input type="text" name="nome">
+            Email: <input type="text" name="email">
+            <input type="submit" value="Cadastrar">
+        </form>
+    ''')
 
 @app.route('/status')
 def status():
     return jsonify({"status": "ok"})
 
-@app.route('/info')
-def info():
-    return "Nossa aplicação é muito boa e concluimos o desafio 3"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
